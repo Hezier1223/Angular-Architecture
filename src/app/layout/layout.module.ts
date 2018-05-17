6cd33c8366ba67bbc4d23e3bc5568ab079a5e2fe
朱正浩 <arthur_zzh@126.com>
2018-05-17 17:45:47 +0800

    init

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PassportComponent } from './passport';
 import { SharedModule } from '@shared/shared.module';
 import { LayoutDefaultComponent } from './default/default.component';
+import { HeaderComponent } from './default/header/header.component';
 
-const COMPONENTS = [LayoutDefaultComponent];
+const COMPONENTS = [LayoutDefaultComponent, HeaderComponent];
 const PASSPORT = [PassportComponent];
 
 @NgModule({
